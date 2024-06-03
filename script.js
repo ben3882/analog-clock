85e2d96ba9158f837de5bc3dcbcd6a53f13ada1e
Ben3882 <benbrown3882@gmail.com>
2024-06-03 00:44:03 -0700

    added js function to add dots to the DOM.

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ let secondHand = document.querySelector('.second-hand');
 let minuteHand = document.querySelector('.minute-hand');
 let hourHand = document.querySelector('.hour-hand');
 let timeDisplay = document.querySelector('.time');
+let container = document.querySelector('.container');
+let dot = document.querySelector('.dot');
 
-
-
+function makeDots () {
+    let dots = [];
+    for( let i = 0; i < 3; i++) {
+        dots[i] = document.createElement('div');
+        container.appendChild(dots[i]);
+        dots[i].className = 'dot';
+        dots[i].style.position = 'absolute';
+        dots[i].style.backgroundColor = 'blue';
+        dots[i].style.top = `${i*10}vw`;
+        dots[i].style.left = `${i*10}vw`;
+        
+    }
+}
+makeDots();
 
 setInterval(()=>{
-    let s;
-    let m;
-    let h;
+    let s, m, h;
     let time = new Date;
     [s, m, h] = convert(time.getSeconds(), time.getMinutes(), time.getHours());
     secondHand.style.transform = `translateX(-50%) rotate(${s}deg)`;
